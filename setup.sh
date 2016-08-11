182b6757174bfd2f35426bad092efcc4e8588cf9
Diwank Singh <diwank.singh@gmail.com>
2016-08-11 19:56:16 +0530

    Updated image dependencies
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Create docker machine
 docker-machine create --driver virtualbox dev
+
+# Pull images
+for image in telegraf influxdb chronograf kapacitor grafana/grafana ansi/mosquitto
+do
+  docker pull $image
+done
+
